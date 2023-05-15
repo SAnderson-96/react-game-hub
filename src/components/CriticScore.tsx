c5dcef5be3f3fa1f4e6220c1e2633d5ef7f66e51
Samuel <samuel.c.anderson@gmail.com>
2023-05-15 15:36:18 -0400

    optimizing images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,7 +1,9 @@
 import { Badge } from "@chakra-ui/react";
+
 interface Props {
   score: number;
 }
+
 function CriticScore({ score }: Props) {
   let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
